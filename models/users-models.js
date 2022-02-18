a11dd765ab62a01cb86a941159267924ad3d9344
thendo90 <thenderson@protonmail.com>
2022-02-18 22:42:35 +0000

    create new endpoint GET /api/users/:username

--- models/users-models.js
+++ models/users-models.js
@@ -1,6 +1,16 @@
 const db = require("../db/connection");
+const { checkIfExists } = require("./check-exists.model");
 
 exports.fetchUsers = async () => {
   const { rows } = await db.query(`SELECT * FROM users;`);
   return rows;
+};
+
+exports.fetchUserById = async (username) => {
+  await checkIfExists("users", username, "username");
+
+  const { rows } = await db.query(`SELECT * FROM users WHERE username = $1`, [
+    username,
+  ]);
+  return rows[0];
 };
--- routes/user-router.js
+++ routes/user-router.js
@@ -1,7 +1,8 @@
-const { getUsers } = require("../controllers/users-controllers");
+const { getUsers, getUserById } = require("../controllers/users-controllers");
 
 const userRouter = require("express").Router();
 
 userRouter.route("/").get(getUsers);
+userRouter.route("/:username").get(getUserById);
 
 module.exports = userRouter;
