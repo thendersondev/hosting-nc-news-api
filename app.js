db99f71ef71f40fa9a0ec2857ed3e40160b09e8c
thendo90 <thenderson@protonmail.com>
2022-03-07 11:05:49 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // controller requiring
@@ -10,6 +11,7 @@ const {
 const apiRouter = require("./routes/api-router");
 // controller requiring
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
